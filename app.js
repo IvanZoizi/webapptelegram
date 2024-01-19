3717ba772700ae6322156f1909ea41c931746271
Ivan Zakharov <95079064+IvanZoizi@users.noreply.github.com>
2024-01-19 21:00:16 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let tg = window.Telegram.WebApp;
 
-let start_reg = document.getElementById("btn1");
-let reg = document.getElementById("btn2");
-
-start_reg.addEventListener("click", () => {
-    document.getElementById("start").style.display = "none";
-    document.getElementById("reg_form").style.display = "block";
+reg.addEventListener("click", () => {
+    tg.close()
 })
