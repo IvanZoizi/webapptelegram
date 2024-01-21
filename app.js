3b0126f634a105c3687427f6e8c9d4844ba01763
Ivan Zakharov <95079064+IvanZoizi@users.noreply.github.com>
2024-01-21 13:18:38 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ tg.MainButton.color = '#2cab37';
 
 let btn1 = document.getElementById("btn2");
 
-btn1.addEventListener("click", function(){
+btn1.addEventListener("click", function() {
+    let name = document.getElementById("user_name").value;
+    let email = document.getElementById("user_email").value;
+    let password = document.getElementById("user_password").value;
+    if (name.length < 5) {
+        document.getElementById("error").innerText = "Ошибка в имени";
+        return;
+    }
+    if (email.length < 8) {
+        document.getElementById("error").innerText = "Ошибка в почте";
+        return;
+    }
+    if (password.length < 8) {
+        document.getElementById("error").innerText = "Ошибка в пароле";
+        return;
+    }
+    let data = {
+        name: name,
+        email: email,
+        password: password
+    }
+    tg.sendData(JSON.stringify(data))
     tg.close()
 });
-
-
